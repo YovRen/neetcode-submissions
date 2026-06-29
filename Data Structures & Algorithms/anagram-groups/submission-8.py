7f8a97bb251f477d8004612bdd5a6a6f9c390023
YovRen <101967233+YovRen@users.noreply.github.com>
2026-06-29 16:19:39 +0800

    Add: anagram-groups - submission-8

---
+++ Data Structures & Algorithms/anagram-groups/submission-8.py
@@ -0,0 +1,9 @@
+class Solution:
+    
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        sps = [(s,''.join(sorted(s))) for s in strs]
+        s_dict = defaultdict(list)
+        for s1,s2 in sps:
+            s_dict[s2].append(s1)
+        return s_list.values
+
