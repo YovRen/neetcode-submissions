a70c7d53cc99a532320f09da68b66ef14bc4154d
YovRen <101967233+YovRen@users.noreply.github.com>
2026-06-29 13:35:44 +0800

    Add: anagram-groups - submission-6

---
+++ Data Structures & Algorithms/anagram-groups/submission-6.py
@@ -0,0 +1,15 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        s_dict = [(s,''.join(sorted(s))) for s in strs]
+        print(s_dict)
+        sps = sorted(s_dict,key=lambda x:x[1])
+        ress = []
+        cur = None
+        for s1,s2 in sps:
+            if s2 != cur:
+                ress.append([s1])
+                cur = s2
+            else:
+                ress[-1].append(s1)
+        return ress
+
