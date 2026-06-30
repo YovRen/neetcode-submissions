5c3b4ee9af63d4a805f377f17deb1d4ab53544a4
YovRen <101967233+YovRen@users.noreply.github.com>
2026-06-30 14:33:54 +0800

    Add: longest-repeating-substring-with-replacement - submission-0

---
+++ Data Structures & Algorithms/longest-repeating-substring-with-replacement/submission-0.py
@@ -0,0 +1,13 @@
+class Solution:
+    def characterReplacement(self, s: str, k: int) -> int:
+        #win容忍的是替换k后只有一种字符的substr
+        #判断方式，win中max_count>=total_count-k
+        win = defaultdict(int)
+        l=0
+        max_len=0
+        for r,char in enumerate(s):
+            win[char]+=1
+            while l<=r and max(win.values())<sum(win.values())-k:
+                l+=1
+            max_len=max(max_len,r-l+1)
+        return max_len
